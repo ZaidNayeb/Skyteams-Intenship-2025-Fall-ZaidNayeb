0f2b3cdc094eeda96cd84e1886ea37d27030864a
Zaid Nayeb <zaid.nayeb11@gmail.com>
2025-11-17 10:43:09 +0430

    Add week_02 answers and tests

---
+++ week_02/src/main/java/question01/Question01.java
@@ -0,0 +1,32 @@
+package question01;
+
+/*
+ 2.	Find the largest and smallest of three numbers
+Description: Read three integers and print the largest and smallest.
+ */
+
+public class Question01 {
+    public static void main(String[] args) {
+        System.out.println(largest(35, 48, -89));
+        System.out.println(smallest(35,48,-89));
+    }
+
+    public static double largest(double a, double b, double c) {
+        double largest = Double.NEGATIVE_INFINITY;
+        double [] numbers = {a, b, c};
+        for (double n : numbers){
+            if (n > largest) largest = n;
+        }
+        return largest;
+    }
+    public static double smallest(double a, double b, double c) {
+        double smallest = Double.POSITIVE_INFINITY;
+        double[] numbers ={a, b, c};
+        for (double n : numbers){
+            if (n < smallest) smallest = n;
+        }
+        return smallest;
+    }
+
+}
+
---
+++ week_02/src/test/java/question01/Question01Test.java
@@ -0,0 +1,61 @@
+package question01;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test class for Question 01
+ *  Find the largest and smallest number between 3 numbers.
+ */
+class Question01Test {
+    
+    @Test
+    void testLargest_01(){
+        assertEquals(50, Question01.largest(35,48,50));
+    }
+    @Test
+    void testLargest_02() {
+        assertEquals(15, Question01.largest(-5, 0, 15));
+    }
+    @Test
+    void testLargest_03() {
+        assertEquals(-3, Question01.largest(-10, -20, -3));
+    }
+    @Test
+    void testLargest_04() {
+        assertEquals(30, Question01.largest(30, 30, 30));
+    }
+
+    @Test
+    void testSmallest_01(){
+        assertEquals(10 , Question01.smallest(10,20,30));
+    }
+    @Test
+    void testSmallest_02() {
+        assertEquals(5, Question01.smallest(20, 5, 30));
+    }
+    @Test
+    void testSmallest_03() {
+        assertEquals(1, Question01.smallest(50, 20, 1));
+    }
+    @Test
+    void testSmallest_04() {
+        assertEquals(7, Question01.smallest(7, 7, 7));
+    }
+    @Test
+    void testSmallest_05() {
+        assertEquals(10, Question01.smallest(10, 10, 50));
+    }
+    @Test
+    void testSmallest_06() {
+        assertEquals(-100, Question01.smallest(-10, -100, -3));
+    }
+
+
+
+
+
+    
+
+}
+
